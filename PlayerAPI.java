a461aa2d979ed303c06e1150b3767f83768181da
marwan-mk2108477 <mk2108477@qu.edu.qa>
2024-05-02 21:00:26 +0300

    Create PlayerAPI.java

---
+++ PlayerAPI.java
@@ -0,0 +1,16 @@
+public interface PlayerAPI {
+    /*
+    Command	Arguments	Actions
+    pseudo	pseudonym	generate new ticket and associate it with pseudonym
+    ticket	ticket	validate received ticket and, if valid, welcome player with pseudonym
+    join	game	add player to game if it exists, otherwise create a new game
+    ready	game	confirm player readiness for game
+    guess	game
+    number	receive player guess for current round of game
+     */
+    void pseudo(String pseudonym);
+    void ticket(String ticket);
+    void join(String game);
+    void ready(String game);
+    void guess(String game, int number);
+}
