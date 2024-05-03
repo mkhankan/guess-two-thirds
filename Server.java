2101f0c1fc118ea6d0e357883f32a385fcaae399
IkingdomI <124034235+IkingdomI@users.noreply.github.com>
2024-05-03 03:01:07 +0300

    added lists and organized server class

--- Server.java
+++ Server.java
@@ -1,15 +1,25 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Server {
     private static final int PORT = 13337;
-    public static void main(String[] args) {
-        ServerSocket serverSocket = null;
-        try {
-            serverSocket = new ServerSocket(PORT);
-            System.out.println("Server started. Waiting for clients...");
+    private ServerSocket serverSocket;
 
+    private List<Player> playersList = Collections.synchronizedList(new ArrayList<>());
+//    private List<Ticket> playersList = Collections.synchronizedList(new ArrayList<>());
+    private List<Game> gamesList = Collections.synchronizedList(new ArrayList<>());
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public void startServer() {
+        System.out.println("Server started. Waiting for clients...");
+        try {
             while (true) {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("New client connected: " + clientSocket);
@@ -29,5 +39,13 @@ public class Server {
                 e.printStackTrace();
             }
         }
+
+    }
+
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(PORT);
+        Server server = new Server(serverSocket);
+        server.startServer();
     }
 }
